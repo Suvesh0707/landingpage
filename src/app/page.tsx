e8500f61e5e63d758c3051bf836a7739b3860cb9
Suvesh0707 <156836161+Suvesh0707@users.noreply.github.com>
2026-04-20 11:40:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/component/Header'
+// import logo from '@/public/images/logo.svg'
+function page() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default page
