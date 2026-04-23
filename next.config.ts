6087a8a7e8a0e3458cfd7e7b22d8012742e463bd
pal606Sandeep <pals23352@gmail.com>
2026-04-23 16:58:24 +0530

    delete the redirectrd process

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['*.ngrok-free.app', '*.ngrok-free.dev', '*.ngrok.io'],
-
-  async redirects() {
-    return [
-      {
-        source: '/landingpage',
-        destination: 'https://mentorcha.us',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
