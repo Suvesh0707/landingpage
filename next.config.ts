d20dfa0c3f9d545a1fb65355b5cecaffe9eb48f3
pal606Sandeep <pals23352@gmail.com>
2026-04-23 16:19:31 +0530

    add redirect in next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['*.ngrok-free.app', '*.ngrok-free.dev', '*.ngrok.io'],
+
+  async redirects() {
+    return [
+      {
+        source: '/landingpage',
+        destination: 'https://mentorcha.us',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
